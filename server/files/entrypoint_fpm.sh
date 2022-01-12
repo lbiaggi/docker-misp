406dcb0029fc815bba54f6c4d1a855de313a0873
Lucas Biaggi <lbjanuario@gmail.com>
2022-01-12 10:04:41 -0300

    Update entrypoint_fpm.sh
    Apply suggestions to use env variables

--- server/files/entrypoint_fpm.sh
+++ server/files/entrypoint_fpm.sh
@@ -4,10 +4,10 @@ change_php_vars(){
     for FILE in /etc/php/*/fpm/php.ini
     do  
         [[ -e $FILE ]] || break
-        sed -i "s/memory_limit = .*/memory_limit = 2048M/" "$FILE"
-        sed -i "s/max_execution_time = .*/max_execution_time = 300/" "$FILE"
-        sed -i "s/upload_max_filesize = .*/upload_max_filesize = 50M/" "$FILE"
-        sed -i "s/post_max_size = .*/post_max_size = 50M/" "$FILE"
+        sed -i "s/memory_limit = .*/memory_limit = $PHP_MEMORY_LIMIT/" "$FILE"
+        sed -i "s/max_execution_time = .*/max_execution_time = $PHP_MAX_EXECUTION_TIME/" "$FILE"
+        sed -i "s/upload_max_filesize = .*/upload_max_filesize = $PHP_UPLOAD_MAX_FILESIZE/" "$FILE"
+        sed -i "s/post_max_size = .*/post_max_size = $PHP_POST_MAX_SIZE/" "$FILE"
     done
 }
 
